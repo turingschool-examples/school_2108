954f820d07fb794c39548cfed63c12979b90c14f
Michael Dao <mike@turing.io>
2021-08-24 13:08:09 -0600

    initial commit

---
+++ spec/school_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+require './lib/school'
+
+RSpec.describe School do
+  context 'Iteration 1' do
+    xit 'exists' do
+      school = School.new('9:00', 7)
+
+      expect(school).to be_a(School)
+    end
+
+    xit 'has a start time' do
+      school = School.new('9:00', 7)
+
+      expect(school.start_time).to eq('9:00')
+    end
+
+    xit 'has hours in a school day' do
+      school = School.new('9:00', 7)
+
+      expect(school.hours_in_school_day).to eq(7)
+    end
+
+    xit 'starts with no student names' do
+      school = School.new('9:00', 7)
+
+      expect(school.student_names).to eq([])
+    end
+  end
+
+  context 'Iteration 2' do
+    xit 'can add student names' do
+      school = School.new('9:00', 7)
+
+      school.add_student_name('Aurora')
+      school.add_student_name('tim')
+      school.add_student_name('megan')
+
+      expect(school.student_names).to eq(['Aurora', 'tim', 'megan'])
+    end
+
+    xit 'can calculate end time' do
+      school_1 = School.new('9:00', 7)
+      school_2 = School.new('9:00', 3)
+
+      expect(school_1.end_time).to eq('16:00')
+      expect(school_2.end_time).to eq('12:00')
+    end
+  end
+end
