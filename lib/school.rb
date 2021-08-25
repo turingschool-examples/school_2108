ff9d9de47dfb6456d0e95c9ce7a1f985343aa5e4
Stephanie Helm <stephaniemhelm@gmail.com>
2021-08-25 11:17:49 -0600

    school solutions

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,32 @@
 class School
+  attr_reader   :start_time,
+                :hours_in_school_day,
+                :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time          = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names       = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time(start_time, hours_in_school_day)
+    (start_time + hours_in_school_day)
+  end
+
+  def is_full_time?(start_time, hours_in_school_day)
+    if hours_in_school_day >= 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names(name)
+    @student_names << name.capitalize
+  end
+
 end
