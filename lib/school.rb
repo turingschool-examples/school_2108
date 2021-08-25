a453bec457bed476337efe221197d246a0d7567c
John Napier <jcee.napier@gmail.com>
2021-08-25 11:19:37 -0600

    Independent challenge 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,26 @@
 class School
+  attr_reader :time, :hours, :name
+
+  def initialize(time, hours)
+    @time = time
+    @hours = hours
+    @name = name
+  end
+
+  def start_time
+    time = "9:00"
+  end
+
+  def hours_in_school_day
+    hours = 7
+  end
+
+  def student_names
+    name = []
+  end
+
+def add_student_name(name)
+   @name = name.map['Aurora', 'tim', 'megan']
+end
+
 end
