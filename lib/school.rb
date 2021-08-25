09bbabe04c4aa64c482df90c043b5ebd5b477c23
Philip Medlin <phmedlin@gmail.com>
2021-08-25 10:55:17 -0600

    Add end time calculator, passing iteration 2 tests

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,13 @@ class School
   attr_reader:start_time,
              :hours_in_school_day
              :student_names
+             :end_time
 
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+    @end_time = end_time
   end
 
   def student_names
@@ -16,5 +18,11 @@ class School
   def add_student_name(name)
     @student_names << name
   end
+
+  def end_time
+    @end_time = ("#{start_time.to_i + hours_in_school_day}:00")
+
+  end
+
 
 end
