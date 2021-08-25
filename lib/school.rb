a5a19e64c08160cb77dd7118c138eb4ee3c9a66c
oscar santos <oscar303data@gmail.com>
2021-08-25 10:33:01 -0600

    Finish iteration 1 exercise

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,22 @@
 class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
+  def initialize(time, hours)
+    @time = time
+    @hours = hours
+  
+  end
+
+  def start_time
+    @time = '9:00'
+  end
+  def hours_in_school_day
+    @hours = 7
+  end
+  def student_names
+    @students = []
+  end
+
 end
