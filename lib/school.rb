17add01e93a03b38e7870bdf5687f21c713e438b
Gregory Fischer <gwhoisj@gmail.com>
2021-08-25 12:27:52 -0400

    Itteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,14 @@ class School
   def add_student_name(name)
     @student_names << name
   end
+
+  def end_time
+    str_time = start_time.gsub(/\D/, "")
+    str_time = str_time.delete("0")
+    int_time = str_time.to_i
+    time_end = int_time + @hours_in_school_day
+    return "#{time_end}:00"
+  end
+  
+
 end
