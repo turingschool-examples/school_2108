4f2551d084381fc506dcae76d2d649f83846d219
Justina Hortizuela <justinahortizuela@gmail.com>
2021-08-25 10:24:24 -0700

    Complete assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,39 @@
 class School
+  attr_reader :time,
+              :hours,
+              :student_names
+  def initialize(time, hours)
+    @time = time
+    @hours = hours
+    @student_names = []
+  end
+
+  def start_time
+    @time
+  end
+
+  def hours_in_school_day
+    @hours
+  end
+
+  def student_names
+    @student_names
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  #def end_time
+
+  def is_school_full_time?
+    if @hours > 4
+      true
+    end
+  end
+
+  def standard_student_names
+    ['Aurora', 'Tim', 'Megan']
+end
+
 end
