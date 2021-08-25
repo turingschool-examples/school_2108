32a12ce77a3f6c7bd26edf3ce2ef559f4c85d04b
Cole Henson <colehenson95@gmail.com>
2021-08-25 11:27:31 -0600

    Create school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,12 @@
 class School
+  attr_reader :start_time,
+  :hours_in_school_day
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
 end
+
+school = School.new('9:00', 7)
