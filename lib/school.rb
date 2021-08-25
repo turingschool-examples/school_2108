95dcb8939bca9503ae6d4d7c91f74b343749be75
Chaz Simons <chaz.simons@gmail.com>
2021-08-25 09:14:14 -0700

    Initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,27 @@
 class School
+  def initialize(start, length)
+    @start = start
+    @length = length
+    @students = []
+  end
+
+  def start_time
+    @start
+  end
+
+  def hours_in_school_day
+    @length
+  end
+
+  def student_names
+    @students
+  end
+
+  def add_student_name(name)
+    @students << name
+  end
+
+  def end_time
+    (@start.to_i + @length).to_s + ":00"
+  end
 end
