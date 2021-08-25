0315b9529330c067d21953f64458772e46df2c04
Paul Leonard <paulpaulleonard@gmail.com>
2021-08-25 13:02:25 -0400

    Finish iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,23 @@
 class School
+  def initialize(school, start_time)
+    @school = school
+    @start_time = '9:00'
+    @student_names = []
+  end
+
+  def start_time
+    "9:00"
+  end
+
+  def hours_in_school_day
+    7
+  end
+
+  def student_names
+    []
+  end
+
+  def add_student_name(student_names)
+    student_names.push('')
+  end
 end
