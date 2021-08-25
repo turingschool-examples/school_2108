9a5f8b05be4d7df506588b6e28a2a10a2ea298b1
Philip Medlin <phmedlin@gmail.com>
2021-08-25 11:28:09 -0600

    Final submission,halfway through iteration 3.

--- lib/school.rb
+++ lib/school.rb
@@ -3,12 +3,14 @@ class School
              :hours_in_school_day
              :student_names
              :end_time
+             :standard_student_names
 
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
     @end_time = end_time
+    @standard_student_names = []
   end
 
   def student_names
@@ -21,8 +23,19 @@ class School
 
   def end_time
     @end_time = ("#{start_time.to_i + hours_in_school_day}:00")
-
   end
 
+  def is_full_time
+    if hours_in_school_day >= 4;
+      true
+    else
+      false
+    end
 
+  def standard_student_names
+    @standard_student_names << student_names.capitalize
+    #@student_names
+  end
+
+end
 end
