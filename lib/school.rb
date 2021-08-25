9e1d44c6cc8905b7ad1c5c4b2205a2b8a6ad23dc
Christian Valesares <cvalesares@gmail.com>
2021-08-25 13:02:19 -0400

    initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,31 @@
 class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
+  def initialize (start_time, hours_in_school_day)
+    @start_time           = start_time
+    @hours_in_school_day  = hours_in_school_day
+    @student_names        = []
+  end
+
+  def add_student_name(name)
+    @student_names.push(name)
+  end
+
+  def end_time
+    "#{@start_time.to_i + @hours_in_school_day}:00"
+  end
+
+  def is_full_time
+    if @hours_in_school_day > 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names
+    @student_names.capitalize 
+  end
 end
