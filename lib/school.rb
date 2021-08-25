48b72806b1d438af455eb5e94bdc66068cbeaef1
Micha Bernhard <mib70@pitt.edu>
2021-08-25 10:19:01 -0600

    Make iteration 2 tests pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,22 @@
 class School
-  attr_reader :start_time, :hours_in_school_day, :student_names
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names,
+              :end_time
 
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+    @end_time = end_time
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    @end_time = ((@hours_in_school_day + @start_time.to_i).to_s + ":00")
   end
 
 end
