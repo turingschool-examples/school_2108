13e0e71633af8b44dbe53499561393ea2d127559
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-25 11:08:49 -0600

    INITIAL COMMIT

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,25 @@
+require 'rspec'
+
 class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+  def initialize(start_time, hours_in_school_day)
+    @start_time           = start_time
+    @hours_in_school_day  = hours_in_school_day
+    @student_names        = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    (@hours_in_school_day + @start_time)
+
 end
+
+school = School.new('9:00', 7)
+school.add_student_name('Aurora')
+school.add_student_name('tim')
+school.add_student_name('megan')
