162cb28528bb4285548c55c918cc1935ca264535
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-25 09:26:39 -0700

    Add iterations 1-3

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,33 @@
 class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    "#{@start_time[0].to_i + @hours_in_school_day}:00"
+  end
+
+  def is_full_time?
+    @hours_in_school_day > 4
+  end
+
+  def standard_student_names
+    @student_names.map do |name|
+      name.capitalize
+    end
+  end
+
+  def convert_end_time_to_clock_time
+    
+  end
+
+
 end
